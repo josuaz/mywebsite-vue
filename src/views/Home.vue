8d9f9bca2f2d60e77d553f369a2b6955a3b399ca
josuaz <josuaaliansa@gmail.com>
2022-08-01 15:35:07 +0800

    Edit hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-blue-600 relative h-screen overflow-hidden"> 
-        <div class="text-white absolute top-1/2 le text-2xl z-10">
-            <h1 class="text-4xl">Hi!</h1>
-            <h1 class="text-2xl">I'm Joshua Aliansa Pranajaya</h1>
+        <div class="text-white absolute top-1/2 le text-2xl z-10 pl-6 lg:pl-24">
+            <h1 class="text-6xl">Hi!</h1>
+            <h1 class="text-2xl pl-1">I'm Joshua Aliansa Pranajaya</h1>
         </div>
         <div class="cube"></div>
         <div class="cube"></div>
