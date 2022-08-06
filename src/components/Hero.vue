1407cc22fd21a07b4b38fd6c3d385dde9cce030c
josuaz <josuaaliansa@gmail.com>
2022-08-06 11:27:39 +0800

    Update hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="overflow-hidden bg-gradient-to-r from-blue-900 to-purple-900 flex flex-row flex-wrap min-h-screen"> 
-        <div class="relative w-full md:w-1/3">
+<div class="overflow-hidden bg-gradient-to-r from-blue-900 to-purple-900 flex flex-row flex-wrap min-h-screen pb-2"> 
+        <div class="relative w-full md:w-1/2">
             <div class="text-white flex flex-col h-full justify-center mb-auto text-2xl pl-6 lg:pl-24 z-[1000] pt-24 pb-16">
-                <h1 class="text-6xl font-bold">Hi!</h1>
-                <h1 class="text-2xl ml-0.5 font-semibold">I'm Joshua Aliansa Pranajaya</h1>
-                <h2 class="flex flex-row pl-0.5 text-2xl">
+                <h1 class="text-6xl font-bold z-10">Hi!</h1>
+                <h1 class="text-2xl ml-0.5 font-semibold z-10">I'm Joshua Aliansa Pranajaya</h1>
+                <h2 class="flex flex-row pl-0.5 text-2xl z-10">
                     I'm a&nbsp;<VueWriter :array="arr" :typeSpeed="70" class="border-r-4 border-white" :eraseSpeed="25" />
                 </h2>
             </div>
@@ -15,41 +15,35 @@
             <div class="cube"></div>
             <div class="cube"></div>
         </div>
-        <div class="flex w-full md:w-2/3 mt-12 md:mt-0 bg-white shadow-lg pt-24 pb-16 pr-6 lg:pr-24" style="clip-path: polygon(39% 0, 100% 0, 100% 100%, 0% 100%);">
-            <img :src="img" class="w-72 my-auto ml-auto rounded-full object-cover z-20" style="" alt="">
+        <div class="w-full md:w-1/2 px-2 lg:pr-24 flex flex-col justify-center">
+            <div class="z-30 flex relative">
+                <button class="p-2 rounded-full bottom-6 left-6 absolute bg-gradient-to-r from-blue-900 to-purple-900 text-white border-neutral-400 border font-semibold hover:from-blue-700 hover:to-purple-700 duration-150">
+                    <span>Hire me!</span>
+                </button>
+                <a href="/" class=" hover:border-black border-b-2 bottom-8 duration-150 left-32 absolute flex font-semibold border-transparent">Project
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-0.5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                    </svg>
+                </a>
+                <img :src="img" class="w-48 h-48 object-cover object-top rounded-full bg-white p-1 ml-auto mr-6" alt="">
+            </div>
+            <div class="p-2 rounded-md bg-white z-20 pt-24 -mt-[5.5rem]">
+                <p class="text-center text-xl font-semibold"> 
+                    Hi, I'm Joshua Aliansa Pranajaya, My experiences covers about:
+                </p>
+                <ul class="list-disc list-inside px-5 text-lg">
+                    <li>2+ years experience in back-end development</li>
+                    <li>1+ year experience in front-end development</li>
+                    <li>1+ year experience digital marketing</li>
+                </ul>
+            </div>
         </div>
 </div>
-<swiper
-    :spaceBetween="30"
-    :centeredSlides="true"
-    :autoplay="{
-      delay: 2500,
-      disableOnInteraction: false,
-    }"
-    :pagination="{
-      clickable: true,
-    }"
-    :navigation="true"
-    :modules="[Autoplay, Pagination, Navigation]"
-    class="mySwiper"
-  >
-    <swiper-slide>
-      
-    </swiper-slide>
-  </swiper>
 </template>
 <script>
-// Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
-
-// Import Swiper styles
 import "swiper/css";
-
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay } from "swiper";
 
 export default {
   components: {
@@ -58,47 +52,13 @@ export default {
   },
   setup() {
     return {
-      modules: [Autoplay, Pagination, Navigation],
+      modules: [Autoplay],
     };
   },
 };
 </script>
 
 <script setup>
-import img from '../assets/img/Joshua.jpg'
+import img from '../assets/img/joshua2.png'
 const arr = ["Web Designer", "Front-end Developer", "Back-end Developer"];
 </script>
-
-<style>
-.swiper {
-  width: 100%;
-  height: 100%;
-}
-
-.swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
-
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="navbar" class="flex text-white fixed w-full px-5 py-2 flex-row z-40">
-        <!-- <button class="p-2 rounded-full block md:hidden hover:bg-white/20 text-white duration-150 focus:border-white border-2 border-transparent">
+        <button class="p-2 rounded-full mr-2 hover:bg-white/20 text-white duration-150 focus:border-white border-2 border-transparent">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h7" />
             </svg>
-        </button> -->
-        <!-- <router-link :to="{name: 'Home'}" class="mx-auto md:mx-0 font-semibold text-xl p-1 rounded-md hover:bg-white-20 duration-150">Joshua</router-link> -->
-        <!-- <ul class="my-auto md:flex flex-row mx-auto hidden">
+        </button>
+        <router-link :to="{name: 'Home'}" class="mx-auto md:mx-0 font-semibold text-xl p-1 rounded-md hover:bg-white-20 duration-150">Joshua</router-link>
+        <ul class="my-auto md:flex flex-row mx-auto hidden">
             <li><router-link :to="{name: 'Home'}" class=" border-b-2 hover:border-white border-transparent duration-150 nav-link font-semibold">Home</router-link></li>
             <li><router-link :to="{name: 'Portfolio'}" class=" border-b-2 hover:border-white border-transparent duration-150 nav-link font-semibold mx-5">Portfolio</router-link></li>
             <li><router-link :to="{name: 'Contact'}" class=" border-b-2 hover:border-white border-transparent duration-150 nav-link font-semibold">Contact</router-link></li>
-        </ul> -->
-        <!-- <button class="p-2 rounded-full border-2 border-white hover:bg-white/20 duration-150 font-semibold text-white">Let's chat</button> -->
+        </ul>
+        <button class="p-2 rounded-full border-2 border-white hover:bg-white/20 duration-150 font-semibold text-white">Let's chat</button>
     </div>
 </template>
 
