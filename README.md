ee902d9e002be4826916ea2de68e1c547b5673ba
josuaz <josuaaliansa@gmail.com>
2022-09-08 18:24:43 +0800

    add dist folder to git

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Personal website
-
-
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import './assets/js/my-script.js'
-import './assets/css/animatedBg.css'
-import VueWriter from "vue-writer";
-import { registerScrollSpy } from 'vue3-scroll-spy';
-
-createApp(App).use(VueWriter).use(registerScrollSpy).mount('#app')
