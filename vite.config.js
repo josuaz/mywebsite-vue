943fa8adf99765404a9da48f3e23e1c6e229c22b
josuaz <josuaaliansa@gmail.com>
2022-09-08 16:03:21 +0800

    change back publicPath to base

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/mywebsite-vue/' :
-        '/',
+    base: '/mywebsite-vue/',
     plugins: [vue()]
 })
